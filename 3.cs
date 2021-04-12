31c838a532bdd25a73ce8ab5f50d48091c8161e7
YaroslawGarbarenko <72256923+YaroslawGarbarenko@users.noreply.github.com>
2021-04-12 14:52:12 +0300

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Console_Application__3._3_
+{
+    class Program
+    {
+        static void Go(object num)
+        {
+            int n = (int)num;
+            if (n <= 0)
+                return;
+            Thread t = new Thread(Go);
+            t.Name = "Thread" + n;
+            t.Start(n - 1);
+            Console.WriteLine(t.Name);
+        }
+        private static void Main(string[] args)
+        {
+            Go(10);
+            Console.ReadKey();
+        }
+    }
+}
